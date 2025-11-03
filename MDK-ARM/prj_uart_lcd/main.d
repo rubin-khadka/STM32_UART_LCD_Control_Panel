fe4a33b914d0460f26fb636f9589bdbe9f5c6be3
rubin-khadka <rubin.robotic@gmail.com>
2025-11-03 15:04:27 +0100

    created necessary changes

--- MDK-ARM/prj_uart_lcd/main.d
+++ MDK-ARM/prj_uart_lcd/main.d
@@ -1,5 +1,5 @@
-prj_uart_lcd/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
-  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
+prj_uart_lcd/main.o: ..\Core\Src\main.c ..\Core\Inc\lcd.h \
+  ..\Core\Inc\main.h ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
   ..\Core\Inc\stm32f1xx_hal_conf.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_def.h \
@@ -26,4 +26,4 @@ prj_uart_lcd/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash_ex.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_uart.h \
-  ..\Core\Inc\lcd.h ..\Core\Inc\uart.h
+  ..\Core\Inc\uart.h
